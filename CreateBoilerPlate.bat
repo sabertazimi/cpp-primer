96b26a920ca5bc880fe141982c074e5be1a2ae66
sabertazimi <sabertazimi@gmail.com>
2021-07-05 21:46:46 +0800

    remove(boilerplate): remove boilerplate code

--- CreateBoilerPlate.bat
+++
@@ -1,8 +0,0 @@
-cd external
-rmdir fmt
-git submodule add --depth 1 -- https://github.com/fmtlib/fmt.git
-rmdir spdlog
-git submodule add --depth 1 -- https://github.com/gabime/spdlog.git
-rmdir doctest
-git submodule add --depth 1 -- https://github.com/onqtam/doctest.git
-cd ..
